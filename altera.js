2384a525d6885f0c1df474d8e332621389829836
Davi Nunes <91471208+kitswu@users.noreply.github.com>
2022-02-04 18:27:11 -0300

    Update altera.js

--- altera.js
+++ altera.js
@@ -3,52 +3,55 @@ f_HExiste()
 function f_HExiste() {
   for (let i =1; i < 7; i++) {
     if (document.querySelector(`h${i}`) != null){
+
       const titulo = document.querySelector(`h${i}`);
+
       titulo.setAttribute('id','titulo');
-      criar(titulo);
+      
+      f_criar(titulo);
     };
   };
 };
 
-function criar(tituloEncontrado) {
+function f_criar(tituloEncontrado) {
 
   const aumenta = document.createElement("button");
   const diminui = document.createElement("button");
 
-  tituloEncontrado.setAttribute("id","titulo") ;
   aumenta.textContent = "+";
-  tituloEncontrado.append(aumenta);
-  aumenta.addEventListener("click", f_aumentarTamanho);
-
-  tituloEncontrado.setAttribute("id","titulo"); 
   diminui.textContent = "-"; 
+
+  tituloEncontrado.append(aumenta);
   tituloEncontrado.append(diminui);
+
+  aumenta.addEventListener("click", f_aumentarTamanho);
   diminui.addEventListener("click", f_diminuirTamanho);
-} 
+};
 
 function iniciar(){
 
   mudar = document.querySelector("#titulo").style;
   mudar.fontSize = "40px";		
-}
+};
 
-function f_aumentarTamanho() { 
-  if (parseInt(document.querySelector("#titulo").style.fontSize) < 110) {
+function f_aumentarTamanho(titulo) { 
 
-      const titulo = document.querySelector("#titulo");
+  const titulo = document.querySelector("#titulo");
+
+  if (parseInt(titulo.style.fontSize) < 110) {
 
       tamanho = parseInt(titulo.style.fontSize) + 10; 
       titulo.style.fontSize = tamanho + "px";
-  }
-} 
+  };
+};
 
-function f_diminuirTamanho() {
+function f_diminuirTamanho(titulo) {
+
+  const titulo = document.querySelector("#titulo");
 
   if (parseInt(document.querySelector("#titulo").style.fontSize) > 25) {
 
-      const titulo = document.querySelector("#titulo");
-
       tamanho = parseInt(titulo.style.fontSize) - 10;   
       titulo.style.fontSize = tamanho + "px"; 
-  }   
-}
+  };   
+};
