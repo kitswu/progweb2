3d895ccd40ead0295a53c9aa7ed11d39661b61c5
Davi Nunes <91471208+kitswu@users.noreply.github.com>
2022-02-04 17:30:38 -0300

    Update Exerc_01.js

--- Exerc_01.js
+++ Exerc_01.js
@@ -1,27 +1,18 @@
-const textos = document.querySelectorAll('h2')
-const sumario = document.querySelector('ol')
-const topicos = []
+const textos = document.querySelectorAll('h2');
+const topicos = new Array();
+textos.forEach(texto => {
+    topicos.push(texto.textContent);
+    texto.innerHTML = `${texto.textContent} <a href='#'>Retornar</a>`;
 
-const newAnchor = (href, text) => {
-    const anchor = document.createElement('a')
-    anchor.href = href
-    anchor.innerHTML = text
-    return anchor
-}
-
-const addReturn = texto => {
-    topicos.push(texto.textContent)
-    const a = newAnchor('#', 'Retornar')
-    texto.innerHTML = texto.textContent
-    texto.append(a)
-}
-
-const addTopic = topico => {
+});
+const sumario = document.querySelector('ol');
+topicos.forEach(topico => {
     const item = document.createElement('li')
-    const a = newAnchor(`#${topico}`, topico)
+    const a = document.createElement('a')
+
+    a.href = `#${topico}`
+    a.innerHTML = topico
+
     item.append(a)
     sumario.append(item)
-}
-
-textos.forEach(addReturn)
-topicos.forEach(addTopic)
+});
